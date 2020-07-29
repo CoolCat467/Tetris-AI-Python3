0e09a283acd1022f18c76798ea3d8c20bfb7e8e8
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2020-07-29 12:25:55 -0500

    Update install-sdl-config.py

--- __tests__/install-sdl-config.py
+++ __tests__/install-sdl-config.py
@@ -11,17 +11,17 @@ __ver_patch__ = 0
 
 import os
 
-commands = ['hg clone https://hg.libsdl.org/SDL SDL',
-            'cd SDL',
-            'mkdir build',
-            'cd build',
-            '../configure',
-            'make',
-            'sudo make install']
-
-for command in commands:
-    out = os.system(command)#'sudo apt-get install libsdl2-2.0 libsdl2-dev')
-    if out != 0:
-        exit(1)
-        break
+#commands = ['hg clone https://hg.libsdl.org/SDL SDL',
+#            'cd SDL',
+#            'mkdir build',
+#            'cd build',
+#            '../configure',
+#            'make',
+#            'sudo make install']
+#
+#for command in commands:
+out = os.system('sudo apt-get install libsdl2-2.0 libsdl2-dev')
+if out != 0:
+    exit(1)
+#        break
 exit(0)
